69ef322b26c41182d15c9e8d9edf7d1e933d5d26
6code <6code384@gmail.com>
2017-10-23 10:11:39 -0300

    eduardo lima

--- app/src/main/java/com/developer/image/rafaela/act/Main2Activity.java
+++ app/src/main/java/com/developer/image/rafaela/act/Main2Activity.java
@@ -1,11 +1,7 @@
 package com.developer.image.rafaela.act;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -395,6 +391,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
     }
+
 
 
 }
---
+++ app/src/main/java/com/developer/image/rafaela/model/CalculaIndices.java
@@ -0,0 +1,44 @@
+package com.developer.image.rafaela.model;
+
+/**
+ * Created by edu on 23/10/17.
+ */
+
+public class CalculaIndices {
+
+
+    private long C10M;
+    private long LPS;
+    private long LPDV;
+    private long VTC;
+    private long LCLC;
+
+
+    public CalculaIndices(long c10M, long LPS, long LPDV, long VTC, long LCLC) {
+
+        C10M = c10M;
+        this.LPS = LPS;
+        this.LPDV = LPDV;
+        this.VTC = VTC;
+        this.LCLC = LCLC;
+    }
+
+    public void calcularC10M(){
+
+    }
+    public void calcularLPS(){
+
+    }
+    public void calcularLPDV(){
+
+    }
+    public void calcularVTC(){
+
+    }
+    public void calcularLCLC(){
+
+    }
+
+
+
+}
